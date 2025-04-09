1c5ef606b5d009dfe693bb18cb586b9db2aeb62f
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-04-09 12:53:18 +0100

    Update server.js

--- src/npm/server.js
+++ src/npm/server.js
@@ -3,9 +3,11 @@ const app = express();
 
 const clientRoutes = require('./client');  
 const homeRoutes = require('./home');
+const projectRoutes = require('./project');
 
 app.use(homeRoutes);
 app.use(clientRoutes);
+app.use(projectRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
